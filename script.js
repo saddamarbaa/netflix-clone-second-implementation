0e92b2b65cce5c043f5e314492b3ab120843dff3
saddamarbaa <saddamarbaas@gmail.com>
2021-01-20 05:32:20 +0700

    ADDDED:fetchMovies() function to fetch data from IMDb API

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //  Selectors
 
 const navigation = document.getElementById("navigation");
-const tvShowsEl = document.getElementById("tvShows");
+const tvShowsEl = document.querySelector("#tvShows");
+const bigMovies = document.querySelector(".big-movies");
 
 // console.log(navigation);
 // console.log(tvShowsEl);
@@ -31,4 +32,34 @@ function addMovies(movies, moveEl) {
     moveEl.innerHTML += image;
   });
 }
-// Functions
+
+// fetch movies data from IMDb API
+
+function fetchMovies() {
+  fetch(
+    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        // throw new Error(response.statusText);
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((data) => {
+      // console.log(data);
+      // call add function and pass movies array
+      addMovies(data.results, tvShowsEl);
+      addMovies(data.results, bigMovies);
+    })
+    .catch((error) => {
+      console.log("Fetch Error :-S", error);
+    });
+}
+
+// function call
+
+window.onload = () => {
+  fetchMovies();
+};
