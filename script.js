fdd8f0690760552a7277c62662000795d1e2eab4
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-26 20:43:09 +0700

    CODE REVIEW:added getPopular() function

--- script.js
+++ script.js
@@ -12,63 +12,13 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const callFetch = function () {
-  const API_KEY = "92bcc12799d8068995c7c9650f414f3e";
-  addTvShows(API_KEY);
-};
+const alreadyDisplayed = [];
 
-const addTvShows = function (API_KEY) {
-  fetch(
-    `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=5000`
-  )
-    .then((response) => {
-      // console.log(response);
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Something went wrong");
-      }
-    })
-    .then((data) => {
-      // console.log(data);
-      displayTvShows(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const getPopular = function () {
+  const popularURL =
+    "https://api.themoviedb.org/3/discover/tv?api_key=92bcc12799d8068995c7c9650f414f3e&sort_by=vote_average.desc&vote_count.gte=5000";
+  callFetch(popularURL, "popular");
 };
-
-const displayTvShows = function (tvShows) {
-  const tvShowsEl = document.getElementById("tvShows");
-  tvShows.results.forEach((tvShow) => {
-    const imgHtml = `<img src="https://image.tmdb.org/t/p/original${tvShow.backdrop_path}" alt="${tvShow.original_name}">`;
-    tvShowsEl.innerHTML += imgHtml;
-  });
-};
-
-const findGenreID = function (genre_name) {
-  fetch(
-    `https://api.themoviedb.org/3/genre/movie/list?api_key=92bcc12799d8068995c7c9650f414f3e&language=en-US`
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Something went wrong!");
-      }
-    })
-    .then((data) => {
-      // console.log(data);
-      const genre_obj = data.genres.filter(
-        (genre) => genre.name === genre_name
-      );
-      console.log(genre_obj);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
 // function calls
 callFetch();
 findGenreID("Drama");
