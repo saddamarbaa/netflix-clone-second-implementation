dee29fa5280e699d46b0a3eaa579ded09e6930d6
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-26 10:15:15 +0700

    CODE REVIEW:ON Javascript file

--- script.js
+++ script.js
@@ -1,101 +1,77 @@
-//  Event Listener and Functions
+// Navigation Scroll feature
+const navigation = document.getElementById("navigation");
 
 window.addEventListener("scroll", () => {
-  //  Selectors
-  const navigation = document.getElementById("navigation");
-  // console.log(navigation);
-  // console.log(document.body.scrollTop);
-  // console.log(document.documentElement.scrollTop);
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    navigation.style.transition = "background 0.2s ease-in";
     navigation.style.background = "rgba(20, 20, 20, 1)";
   } else {
-    navigation.style.background = "rgba(20, 20, 20, 0.1)";
+    navigation.style.transition = "background 0.4s 0.15s";
+    navigation.style.background =
+      "linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 10%, rgba(0, 0, 0, 0)";
   }
 });
 
-//  Add Movies to the front end
-
-const displayTvShows = function (tvShows) {
-  // console.log(tvShows);
-  const tvShowsEl = document.getElementById("tvShows");
-
-  tvShows.results.forEach((tvShow) => {
-    const imgHtml = `<img src="https://image.tmdb.org/t/p/original${tvShow.backdrop_path}" alt="${tvShow.original_name}">`;
-
-    // console.log(imgHtml);
-    tvShowsEl.innerHTML += imgHtml;
-  });
-};
-
-//  Add Movies to the front end
-
-const displayNetflixOriginal = function (tvShows) {
-  console.log(tvShows);
-  const tvShowsEl = document.getElementById("netflixOriginal");
-
-  tvShows.results.forEach((tvShow) => {
-    const imgHtml = `<img src="https://image.tmdb.org/t/p/original${tvShow.backdrop_path}" alt="${tvShow.original_name}">`;
-
-    console.log(imgHtml);
-    tvShowsEl.innerHTML += imgHtml;
-  });
-};
-
 const callFetch = function () {
   const API_KEY = "92bcc12799d8068995c7c9650f414f3e";
-  fetchMovies(API_KEY);
-  fetchOrginalMovies(API_KEY);
+  addTvShows(API_KEY);
 };
 
-// fetch movies data from IMDb API
-
-const fetchMovies = function (API_KEY) {
-  const url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=5000`;
-
-  fetch(url)
+const addTvShows = function (API_KEY) {
+  fetch(
+    `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=5000`
+  )
     .then((response) => {
+      // console.log(response);
       if (response.ok) {
         return response.json();
       } else {
-        // throw new Error(response.statusText);
         throw new Error("Something went wrong");
       }
     })
     .then((data) => {
       // console.log(data);
-      // call displayTvShows() function and pass movies Object
       displayTvShows(data);
-      // displayNetflixOriginal(data);
     })
     .catch((error) => {
-      console.log("Fetch Error :-S", error);
+      console.log(error);
     });
 };
 
-// function call
-
-window.onload = () => {
-  callFetch();
+const displayTvShows = function (tvShows) {
+  const tvShowsEl = document.getElementById("tvShows");
+  tvShows.results.forEach((tvShow) => {
+    const imgHtml = `<img src="https://image.tmdb.org/t/p/original${tvShow.backdrop_path}" alt="${tvShow.original_name}">`;
+    tvShowsEl.innerHTML += imgHtml;
+  });
 };
 
-function fetchOrginalMovies(API_KEY) {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1`;
-
-  fetch(url)
+const findGenreID = function (genre_name) {
+  fetch(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=92bcc12799d8068995c7c9650f414f3e&language=en-US`
+  )
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
-        // throw new Error(response.statusText);
-        throw new Error("Something went wrong");
+        throw new Error("Something went wrong!");
       }
     })
     .then((data) => {
       // console.log(data);
-      // call displayTvShows() function and pass movies Object
-      displayNetflixOriginal(data);
+      const genre_obj = data.genres.filter(
+        (genre) => genre.name === genre_name
+      );
+      console.log(genre_obj);
     })
     .catch((error) => {
-      console.log("Fetch Error :-S", error);
+      console.log(error);
     });
-}
+};
+
+// function calls
+callFetch();
+findGenreID("Drama");
+
+// Giving back the GENRES
+//  https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US
